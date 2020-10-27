6848b2711ca7b0aa465b6c14e6190eb9a92013f6
Seungkyun Hong (tbox-K1805) <xo@kisti.re.kr>
2020-10-27 11:56:56 +0900

    ROLLBACK

--- condaenv-exporter/condaenv-adapter.py
+++ condaenv-exporter/condaenv-adapter.py
@@ -1,4 +1,3 @@
-import os
 import sys
 import subprocess
 from functools import partial
@@ -10,11 +9,6 @@ from functools import partial
 
 def main():
 
-    try:
-        logfile_path = os.environ['LOGFILE']
-    except KeyError:
-        logfile_path = None
-    print("os.environ['LOGFILE']: {:}".format(logfile_path))
     print("Exec: {:}".format(str(sys.argv)))
     if len(sys.argv) > 1:
         try:
@@ -22,17 +16,11 @@ def main():
             subproc = fn_subproc(sys.argv[1:])
             print("[D] STDOUT:\n{:}".format(subproc.stdout))
             print("[D] STDERR:\n{:}".format(subproc.stderr))
-            if logfile_path is not None:
-                with open(logfile_path, mode='w') as f:
-                    f.write(subproc.stdout)
-                    f.flush()
-                    f.close()
         except (subprocess.SubprocessError, subprocess.CalledProcessError) as e:
             print("[E] error occurred while running:\nExec: {:}\n{:}".format(str(sys.argv[1:]), str(e)))
     else:
         pass
 
-    # fin
     sys.exit(0)
 
 
