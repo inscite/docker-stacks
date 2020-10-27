cae8daa7c524df4412fcf5a68b4d1c97451231f3
Seungkyun Hong (tbox-K1805) <xo@kisti.re.kr>
2020-10-27 16:35:51 +0900

    revision for exporter debugging

--- condaenv-exporter/bootstrap.sh
+++ condaenv-exporter/bootstrap.sh
@@ -20,7 +20,7 @@ if [ "${EXPORTMODE}" == "VERIFY" ]; then
     su -c "${CONDAEVAL};python /opt/condaenv-export-validation.py ${SRCENVNAME} ${DSTENVNAME};" $NB_USER
     su -c "rm -rf /opt/conda/pubenvs/${DSTENVNAME};" $NB_USER
 elif [ "${EXPORTMODE}" == "EXPORT" ]; then
-    su -c "${CONDAEVAL};"'conda create --clone "$SRCENVNAME" -p /opt/conda/pubenvs/"${DSTENVNAME}";' $NB_USER
+    su -c "${CONDAEVAL};"'ls -alh /opt/conda/pubenvs/;df -h;conda create --clone "$SRCENVNAME" -p /opt/conda/pubenvs/"${DSTENVNAME}";' $NB_USER
 elif [ "${EXPORTMODE}" == "QUERYENVS" ]; then
     su -c "${CONDAEVAL};python /opt/condaenv-adapter.py conda env list;" $NB_USER
 elif [ "${EXPORTMODE}" == "QUERYPKGS" ]; then
