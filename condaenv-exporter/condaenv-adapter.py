1562f32129a813f9409efa8178789b3c795af67d
Seungkyun Hong (tbox-K1805) <xo@kisti.re.kr>
2020-10-27 11:39:04 +0900

    explicit logging & code cleaning

--- condaenv-exporter/condaenv-adapter.py
+++ condaenv-exporter/condaenv-adapter.py
@@ -1,27 +1,40 @@
+import os
 import sys
 import subprocess
+from functools import partial
 
 """Copyright (c) 2020 Seungkyun Hong. <nah@kakao.com>
    Distributed under the terms of the 3-Clause BSD License.
 """
 
+
 def main():
 
+    try:
+        logfile_path = os.environ['LOGFILE']
+    except KeyError:
+        logfile_path = None
+    print("os.environ['LOGFILE']: {:}".format(logfile_path))
     print("Exec: {:}".format(str(sys.argv)))
     if len(sys.argv) > 1:
         try:
-            subproc = subprocess.run(sys.argv[1:], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="UTF-8")
-#            print(subproc.stdout, file=sys.stdout)
-#            print(subproc.stderr, file=sys.stderr)
+            fn_subproc = partial(subprocess.run, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='UTF-8')
+            subproc = fn_subproc(sys.argv[1:])
             print("[D] STDOUT:\n{:}".format(subproc.stdout))
             print("[D] STDERR:\n{:}".format(subproc.stderr))
+            if logfile_path is not None:
+                with open(logfile_path, mode='w') as f:
+                    f.write(subproc.stdout)
+                    f.flush()
+                    f.close()
         except (subprocess.SubprocessError, subprocess.CalledProcessError) as e:
             print("[E] error occurred while running:\nExec: {:}\n{:}".format(str(sys.argv[1:]), str(e)))
     else:
         pass
 
+    # fin
     sys.exit(0)
-    return
+
 
 if __name__ == "__main__":
     main()
--- condaenv-exporter/condaenv-export-validation.py
+++ condaenv-exporter/condaenv-export-validation.py
@@ -1,10 +1,12 @@
 import os
 import sys
 import subprocess
+from functools import partial
 
 """Copyright (c) 2020 Seungkyun Hong. <nah@kakao.com>
    Distributed under the terms of the 3-Clause BSD License.
 """
+
 
 def refine_pkgs_info(inst, join=True):
     if not inst.startswith('#'):
@@ -13,10 +15,12 @@ def refine_pkgs_info(inst, join=True):
     else:
         return None
 
+
 def main():
-    
     SRCENVNAME = sys.argv[1]
     DSTENVNAME = sys.argv[2]
+
+    fn_subproc = partial(subprocess.run, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='UTF-8')
 
     # 00. get-prefix
     prefix_info = {}
@@ -35,43 +39,30 @@ def main():
     condaenv_dst = {"cmd": "conda list -n {:}".format(DSTENVNAME).split(' ')}
 
     try:
-#        condaenv_src.update({"proc": subprocess.run(condaenv_src["cmd"], capture_output=True)})
-        condaenv_src.update({"proc": subprocess.run(condaenv_src["cmd"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)})
+        condaenv_src.update({'proc': fn_subproc(condaenv_src['cmd'])})
         condaenv_src.update({
-            "stdout": condaenv_src["proc"].stdout.decode("utf-8").strip(),
-            "stderr": condaenv_src["proc"].stderr.decode("utf-8").strip(),
+            'stdout': condaenv_src['proc'].stdout,
+            'stderr': condaenv_src['proc'].stderr,
         })
     except (subprocess.SubprocessError, subprocess.CalledProcessError) as e:
         condaenv_src.update({
-            "stdout": "",
-	    "stderr": "[E] error occurred while retrieving condaenv-src\nExec: {:}\n{:}".format(condaenv_src["cmd"], str(e)),
+            'stdout': '',
+            'stderr': "[E] error occurred while retrieving condaenv-src\nExec: {:}\n{:}".format(condaenv_src['cmd'],
+                                                                                                str(e)),
         })
-    
+
     try:
-#        condaenv_dst.update({"proc": subprocess.run(condaenv_dst["cmd"], capture_output=True)})
-        condaenv_dst.update({"proc": subprocess.run(condaenv_dst["cmd"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)})
+        condaenv_dst.update({'proc': fn_subproc(condaenv_dst['cmd'])})
         condaenv_dst.update({
-            "stdout": condaenv_dst["proc"].stdout.decode("utf-8").strip(),
-            "stderr": condaenv_dst["proc"].stderr.decode("utf-8").strip(),
+            'stdout': condaenv_dst['proc'].stdout,
+            'stderr': condaenv_dst['proc'].stderr,
         })
     except (subprocess.SubprocessError, subprocess.CalledProcessError) as e:
         condaenv_dst.update({
-            "stdout": "",
-	    "stderr": "[E] error occurred while retrieving condaenv-dst\nExec: {:}\n{:}".format(condaenv_dst["cmd"], str(e)),
+            'stdout': '',
+            'stderr': "[E] error occurred while retrieving condaenv-dst\nExec: {:}\n{:}".format(condaenv_dst['cmd'],
+                                                                                                str(e)),
         })
-
-#    condaenv_src.update({"proc": subprocess.run(condaenv_src["cmd"], capture_output=True)})
-#    condaenv_dst.update({"proc": subprocess.run(condaenv_dst["cmd"], capture_output=True)})
-
-#    condaenv_src.update({
-#        "stdout": condaenv_src["proc"].stdout.decode("utf-8").strip(),
-#        "stderr": condaenv_src["proc"].stderr.decode("utf-8").strip(),
-#    })
-
-#    condaenv_dst.update({
-#        "stdout": condaenv_dst["proc"].stdout.decode("utf-8").strip(),
-#        "stderr": condaenv_dst["proc"].stderr.decode("utf-8").strip(),
-#    })
 
     src_conda_set = set(filter(None, [refine_pkgs_info(inst) for inst in condaenv_src["stdout"].split('\n')]))
     dst_conda_set = set(filter(None, [refine_pkgs_info(inst) for inst in condaenv_dst["stdout"].split('\n')]))
@@ -83,43 +74,30 @@ def main():
     pip_dst = {"cmd": "{:}/bin/pip freeze".format(prefix_info[DSTENVNAME]).split(' ')}
 
     try:
-#        pip_src.update({"proc": subprocess.run(pip_src["cmd"], capture_output=True)})
-        pip_src.update({"proc": subprocess.run(pip_src["cmd"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)})
+        pip_src.update({'proc': fn_subproc(pip_src['cmd'])})
         pip_src.update({
-            "stdout": pip_src["proc"].stdout.decode("utf-8").strip(),
-            "stderr": pip_src["proc"].stderr.decode("utf-8").strip(),
+            'stdout': pip_src['proc'].stdout,
+            'stderr': pip_src['proc'].stderr,
         })
     except (subprocess.SubprocessError, subprocess.CalledProcessError) as e:
         pip_src.update({
-            "stdout": "",
-            "stderr": "[E] error occurred while retrieving pipfreeze-src\nExec: {:}\n{:}".format(pip_src["cmd"], str(e)),
+            'stdout': '',
+            'stderr': "[E] error occurred while retrieving pipfreeze-src\nExec: {:}\n{:}".format(pip_src['cmd'],
+                                                                                                 str(e)),
         })
 
     try:
-#        pip_dst.update({"proc": subprocess.run(pip_dst["cmd"], capture_output=True)})
-        pip_dst.update({"proc": subprocess.run(pip_dst["cmd"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)})
+        pip_dst.update({'proc': fn_subproc(pip_dst['cmd'])})
         pip_dst.update({
-            "stdout": pip_dst["proc"].stdout.decode("utf-8").strip(),
-            "stderr": pip_dst["proc"].stderr.decode("utf-8").strip(),
+            'stdout': pip_dst['proc'].stdout,
+            'stderr': pip_dst['proc'].stderr,
         })
     except (subprocess.SubprocessError, subprocess.CalledProcessError) as e:
         pip_dst.update({
-            "stdout": "",
-            "stderr": "[E] error occurred while retrieving pipfreeze-dst\nExec: {:}\n{:}".format(pip_dst["cmd"], str(e)),
+            'stdout': '',
+            'stderr': "[E] error occurred while retrieving pipfreeze-dst\nExec: {:}\n{:}".format(pip_dst['cmd'],
+                                                                                                 str(e)),
         })
-        
-#    pip_src.update({"proc": subprocess.run(pip_src["cmd"], capture_output=True)})
-#    pip_dst.update({"proc": subprocess.run(pip_dst["cmd"], capture_output=True)})
-
-#    pip_src.update({
-#        "stdout": pip_src["proc"].stdout.decode("utf-8").strip(),
-#        "stderr": pip_src["proc"].stderr.decode("utf-8").strip(),
-#    })
-
-#    pip_dst.update({
-#        "stdout": pip_dst["proc"].stdout.decode("utf-8").strip(),
-#        "stderr": pip_dst["proc"].stderr.decode("utf-8").strip(),
-#    })
 
     src_pip_set = set(filter(None, [refine_pkgs_info(inst) for inst in pip_src["stdout"].split('\n')]))
     dst_pip_set = set(filter(None, [refine_pkgs_info(inst) for inst in pip_dst["stdout"].split('\n')]))
@@ -131,43 +109,28 @@ def main():
     py_dst = {"cmd": "{:}/bin/python /opt/chk_py_ver.py".format(prefix_info[DSTENVNAME]).split(' ')}
 
     try:
-#        py_src.update({"proc": subprocess.run(py_src["cmd"], capture_output=True)})
-        py_src.update({"proc": subprocess.run(py_src["cmd"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)})
+        py_src.update({'proc': fn_subproc(py_src['cmd'])})
         py_src.update({
-            "stdout": py_src["proc"].stdout.decode("utf-8").strip(),
-            "stderr": py_src["proc"].stderr.decode("utf-8").strip(),
+            'stdout': py_src['proc'].stdout,
+            'stderr': py_src['proc'].stderr,
         })
     except (subprocess.SubprocessError, subprocess.CalledProcessError) as e:
         py_src.update({
-            "stdout": "py-chk-src-err",
-            "stderr": "[E] error occurred while retrieving pychk-src\nExec: {:}\n{:}".format(py_src["cmd"], str(e)),
+            'stdout': 'py-chk-src-err',
+            'stderr': "[E] error occurred while retrieving pychk-src\nExec: {:}\n{:}".format(py_src['cmd'], str(e)),
         })
 
     try:
-#        py_dst.update({"proc": subprocess.run(py_dst["cmd"], capture_output=True)})
-        py_dst.update({"proc": subprocess.run(py_dst["cmd"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)})
+        py_dst.update({'proc': fn_subproc(py_dst['cmd'])})
         py_dst.update({
-            "stdout": py_dst["proc"].stdout.decode("utf-8").strip(),
-            "stderr": py_dst["proc"].stderr.decode("utf-8").strip(),
+            'stdout': py_dst['proc'].stdout,
+            'stderr': py_dst['proc'].stderr,
         })
     except (subprocess.SubprocessError, subprocess.CalledProcessError) as e:
         py_dst.update({
-            "stdout": "py-chk-dst-err",
-            "stderr": "[E] error occurred while retrieving pychk-dst\nExec: {:}\n{:}".format(py_dst["cmd"], str(e)),
+            'stdout': 'py-chk-dst-err',
+            'stderr': "[E] error occurred while retrieving pychk-dst\nExec: {:}\n{:}".format(py_dst['cmd'], str(e)),
         })
-
-#    py_src.update({"proc": subprocess.run(py_src["cmd"], capture_output=True)})
-#    py_dst.update({"proc": subprocess.run(py_dst["cmd"], capture_output=True)})
-
-#    py_src.update({
-#        "stdout": py_src["proc"].stdout.decode("utf-8").strip(),
-#        "stderr": py_src["proc"].stderr.decode("utf-8").strip(),
-#    })
-
-#    py_dst.update({
-#        "stdout": py_dst["proc"].stdout.decode("utf-8").strip(),
-#        "stderr": py_dst["proc"].stderr.decode("utf-8").strip(),
-#    })
 
     chk03 = py_src["stdout"] == py_dst["stdout"]
     print("pychk-equal?{:}".format(chk03))
@@ -178,6 +141,7 @@ def main():
 
     sys.exit(0)
     return
+
 
 if __name__ == "__main__":
     main()
