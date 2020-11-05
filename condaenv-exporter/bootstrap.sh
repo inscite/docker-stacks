1a50be5a8daace700c6c8b31051bf13569999d2a
Seungkyun Hong (tbox-K1805) <xo@kisti.re.kr>
2020-11-05 11:10:57 +0900

    revised default threadpool policy in conda tasks (1->10) & cpu benchmark

--- condaenv-exporter/bootstrap.sh
+++ condaenv-exporter/bootstrap.sh
@@ -12,21 +12,35 @@ chown -R $NB_UID:$NB_GID /home/$NB_USER
 CONDAEVAL='eval "$(/opt/conda/bin/conda shell.bash hook)"'
 su -c "${CONDAEVAL};conda config --prepend envs_dirs /opt/conda/pubenvs" $NB_USER
 su -c "${CONDAEVAL};conda config --prepend envs_dirs /home/${NB_USER}/miniconda3/envs" $NB_USER
+
+# threading
+# reference: https://github.com/conda/conda/releases/tag/4.7.11
+if [[ ! -z ${CONDA_DEFAULT_THREADS} ]]; then
+    su -c "${CONDAEVAL};conda config --set default_threads ${CONDA_DEFAULT_THREADS}" $NB_USER
+fi
+
+# ownership take
 chown -R $NB_UID:$NB_GID /home/$NB_USER
+
+if [ "${LAP}" == "TRUE" ]; then
+    EXECMD="time su -c"
+else
+    EXECMD="su -c"
+fi
 
 # (verify|export) conda-env clone
 if [ "${EXPORTMODE}" == "VERIFY" ]; then
-    su -c "${CONDAEVAL};"'conda create --clone "$SRCENVNAME" -p /opt/conda/pubenvs/"${DSTENVNAME}";' $NB_USER
-    su -c "${CONDAEVAL};python /opt/condaenv-export-validation.py ${SRCENVNAME} ${DSTENVNAME};" $NB_USER
-    su -c "rm -rf /opt/conda/pubenvs/${DSTENVNAME};" $NB_USER
+    ${EXECMD} "${CONDAEVAL};"'conda create --clone "$SRCENVNAME" -p /opt/conda/pubenvs/"${DSTENVNAME}";' $NB_USER
+    ${EXECMD} "${CONDAEVAL};python /opt/condaenv-export-validation.py ${SRCENVNAME} ${DSTENVNAME};" $NB_USER
+    ${EXECMD} "rm -rf /opt/conda/pubenvs/${DSTENVNAME};" $NB_USER
 elif [ "${EXPORTMODE}" == "EXPORT" ]; then
-    su -c "${CONDAEVAL};"'ls -alh /opt/conda/pubenvs/;df -h;conda create --clone "$SRCENVNAME" -p /opt/conda/pubenvs/"${DSTENVNAME}";' $NB_USER
+    ${EXECMD} "${CONDAEVAL};"'ls -alh /opt/conda/pubenvs/;df -h;conda create --clone "$SRCENVNAME" -p /opt/conda/pubenvs/"${DSTENVNAME}";' $NB_USER
 elif [ "${EXPORTMODE}" == "QUERYENVS" ]; then
-    su -c "${CONDAEVAL};python /opt/condaenv-adapter.py conda env list;" $NB_USER
+    ${EXECMD} "${CONDAEVAL};python /opt/condaenv-adapter.py conda env list;" $NB_USER
 elif [ "${EXPORTMODE}" == "QUERYPKGS" ]; then
-    su -c "${CONDAEVAL};python /opt/condaenv-adapter.py conda list -n ${SRCENVNAME};" $NB_USER
+    ${EXECMD} "${CONDAEVAL};python /opt/condaenv-adapter.py conda list -n ${SRCENVNAME};" $NB_USER
 elif [ "${EXPORTMODE}" == "WETRUN" ]; then
-    su -c "${CONDAEVAL};conda activate ${SRCENVNAME};LOGFILE="${LOGFILE}" python /opt/condaenv-adapter.py "'${WETRUNARGS};' $NB_USER
+    ${EXECMD} "${CONDAEVAL};conda activate ${SRCENVNAME};LOGFILE="${LOGFILE}" python /opt/condaenv-adapter.py "'${WETRUNARGS};' $NB_USER
 else
     echo 
 fi
