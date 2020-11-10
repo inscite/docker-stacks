c40169a47d88f46a6c295d64b6c8b3d07ef54bd3
xo (K1805P2) <nah@kakao.com>
2020-11-10 11:33:17 +0900

    hotfix for condaenv logging (currently blocking results)

--- condaenv-exporter/bootstrap.sh
+++ condaenv-exporter/bootstrap.sh
@@ -9,21 +9,24 @@ usermod -aG 100 $NB_USER
 cp -R /home/dataon /home/$NB_USER
 chown -R $NB_UID:$NB_GID /home/$NB_USER
 
-CONDAEVAL='eval "$(/opt/conda/bin/conda shell.bash hook)"'
-su -c "${CONDAEVAL};conda config --prepend envs_dirs /opt/conda/pubenvs" $NB_USER
-su -c "${CONDAEVAL};conda config --prepend envs_dirs /home/${NB_USER}/miniconda3/envs" $NB_USER
+CONDAEVAL='eval "$(/opt/conda/bin/conda shell.bash hook)";'
+# very first conda init
+su -c "${CONDAEVAL}conda init bash >/dev/null 2>&1;" $NB_USER
+
+su -c "${CONDAEVAL}conda config --prepend envs_dirs /opt/conda/pubenvs" $NB_USER
+su -c "${CONDAEVAL}conda config --prepend envs_dirs /home/${NB_USER}/miniconda3/envs" $NB_USER
 
 # threading
 # reference: https://github.com/conda/conda/releases/tag/4.7.11
 if [[ ! -z ${CONDA_DEFAULT_THREADS} ]]; then
-    su -c "${CONDAEVAL};conda config --set default_threads ${CONDA_DEFAULT_THREADS}" $NB_USER
+    su -c "${CONDAEVAL}conda config --set default_threads ${CONDA_DEFAULT_THREADS}" $NB_USER
 fi
 
 if [ "${ENABLESSHD}" == "TRUE" ]; then
     service ssh start
 fi
 
-# ownership take
+# final ownership take
 chown -R $NB_UID:$NB_GID /home/$NB_USER
 
 if [ "${LAP}" == "TRUE" ]; then
@@ -34,8 +37,9 @@ fi
 
 # (verify|export) conda-env clone
 if [ "${EXPORTMODE}" == "VERIFY" ]; then
-    ${EXECMD} "${CONDAEVAL};"'conda create --clone "$SRCENVNAME" -p /opt/conda/pubenvs/"${DSTENVNAME}";' $NB_USER
-    ${EXECMD} "${CONDAEVAL};python /opt/condaenv-export-validation.py ${SRCENVNAME} ${DSTENVNAME};" $NB_USER
+    VERIF_FINALOUT="/tmp/verification.out"
+    ${EXECMD} "${CONDAEVAL}"'conda create --clone "$SRCENVNAME" -p /opt/conda/pubenvs/"${DSTENVNAME}";' $NB_USER
+    ${EXECMD} "${CONDAEVAL}python /opt/condaenv-export-validation.py ${SRCENVNAME} ${DSTENVNAME};" $NB_USER
     while :;
     do
         [ ! -d "/opt/conda/pubenvs/${DSTENVNAME}" ] && break
@@ -44,17 +48,23 @@ if [ "${EXPORTMODE}" == "VERIFY" ]; then
         sleep 1
     done
     echo "----------"
-    cat "/tmp/verification.out"
+    cat "${VERIF_FINALOUT}"
+    rm -f "${VERIF_FINALOUT}"
 elif [ "${EXPORTMODE}" == "EXPORT" ]; then
-    ${EXECMD} "${CONDAEVAL};"'conda create --clone "$SRCENVNAME" -p /opt/conda/pubenvs/"${DSTENVNAME}";' $NB_USER
+    ${EXECMD} "${CONDAEVAL}"'conda create --clone "$SRCENVNAME" -p /opt/conda/pubenvs/"${DSTENVNAME}";' $NB_USER
 elif [ "${EXPORTMODE}" == "QUERYENVS" ]; then
-    ${EXECMD} "${CONDAEVAL};python /opt/condaenv-adapter.py conda env list;" $NB_USER
+    ${EXECMD} "${CONDAEVAL}python /opt/condaenv-adapter.py conda env list;" $NB_USER
 elif [ "${EXPORTMODE}" == "QUERYPKGS" ]; then
-    ${EXECMD} "${CONDAEVAL};python /opt/condaenv-adapter.py conda list -n ${SRCENVNAME};" $NB_USER
+    ${EXECMD} "${CONDAEVAL}python /opt/condaenv-adapter.py conda list -n ${SRCENVNAME};" $NB_USER
 elif [ "${EXPORTMODE}" == "WETRUN" ]; then
-    ${EXECMD} "${CONDAEVAL};conda activate ${SRCENVNAME};LOGFILE="${LOGFILE}" python /opt/condaenv-adapter.py "'${WETRUNARGS};' $NB_USER
+    if [ "${SRCENVNAME}" != "-" ] && [ "${SRCENVNAME}" != "_" ] && [ "${SRCENVNAME}" != "" ] && [ ! -z "${SRCENVNAME}" ]; then
+        CONDA_ACTIVATE="conda activate ${SRCENVNAME};"
+    else
+        CONDA_ACTIVATE=""
+    fi
+    ${EXECMD} "${CONDAEVAL}${CONDA_ACTIVATE}LOGFILE="${LOGFILE}" python /opt/condaenv-adapter.py "'${WETRUNARGS};' $NB_USER
 elif [ "${EXPORTMODE}" == "MOUNTINFO" ]; then
-    ${EXECMD} "${CONDAEVAL};"'ls -alh /opt/conda/pubenvs/;df -h;";' $NB_USER
+    ${EXECMD} "${CONDAEVAL}"'ls -alh /opt/conda/pubenvs/;df -h;";' $NB_USER
 else
     echo 
 fi
--- condaenv-exporter/condaenv-adapter.py
+++ condaenv-exporter/condaenv-adapter.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from subprocess import run as sprun, PIPE as spPIPE, SubprocessError, CalledProcessError
+from subprocess import run as sprun, PIPE as spPIPE, CalledProcessError, SubprocessError, TimeoutExpired
 from functools import partial
 
 """Copyright (c) 2020 Seungkyun Hong. <nah@kakao.com>
@@ -17,24 +17,28 @@ def main():
         logfile_path = None
     print("os.environ['LOGFILE']: {:}".format(logfile_path))
 
+    # subproc_result = None
     if len(sys.argv) > 1:
         try:
             fn_subproc = partial(sprun, check=True, stdout=spPIPE, stderr=spPIPE, encoding='UTF-8')
-            subproc = fn_subproc(sys.argv[1:])
-            print("[D] STDOUT:\n{:}".format(subproc.stdout))
-            print("[D] STDERR:\n{:}".format(subproc.stderr))
+            subproc_result = fn_subproc(sys.argv[1:])
 
-            if logfile_path is not None and logfile_path != '' and logfile_path != '-':
-                with open(logfile_path, mode='w') as f:
-                    f.write("=====[STDOUT]=====\n")
-                    f.write(subproc.stdout)
-                    f.flush()
-                    f.write("=====[STDERR]=====\n")
-                    f.write(subproc.stderr)
-                    f.flush()
-                    f.close()
-        except (SubprocessError, CalledProcessError) as e:
+        except (CalledProcessError, SubprocessError, TimeoutExpired) as e:
             print("[E] error occurred while running:\nExec: {:}\n{:}".format(str(sys.argv[1:]), str(e)))
+            subproc_result = e
+
+        print("[D] STDOUT:\n{:}".format(subproc_result.stdout))
+        print("[D] STDERR:\n{:}".format(subproc_result.stderr))
+
+        if logfile_path is not None and logfile_path != '' and logfile_path != '-':
+            with open(logfile_path, mode='w') as f:
+                f.write("=====[STDOUT]=====\n")
+                f.write(subproc_result.stdout)
+                f.flush()
+                f.write("=====[STDERR]=====\n")
+                f.write(subproc_result.stderr)
+                f.flush()
+                f.close()
     else:
         pass
 
